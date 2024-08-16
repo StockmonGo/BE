d15047e776e908493c6f1b03b77627b0cb02b0b9
ChaeyoonWoo <heeni1004@gmail.com>
2024-08-16 10:48:09 +0900

    Stockmon-be initial setting

---
+++ commons/src/main/java/com/pda/commons/rabbitmq/RabbitmqConfig.java
@@ -0,0 +1,4 @@
+package com.pda.commons.rabbitmq;
+
+public class RabbitmqConfig {
+}
---
+++ src/main/java/com/pda/stockmon_be/StockmonBeApplication.java
@@ -0,0 +1,13 @@
+package com.pda.stockmon_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmonBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmonBeApplication.class, args);
+	}
+
+}
