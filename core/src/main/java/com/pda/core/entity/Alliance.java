e6ab23e2b65c93e53fd47242ee07890cfa88849f
ChaeyoonWoo <heeni1004@gmail.com>
2024-08-22 15:30:26 +0900

    feat: Traveler, Alliance, Account, TravelerAlliance 엔티티 추가
    #1

---
+++ core/src/main/java/com/pda/core/entity/Alliance.java
@@ -0,0 +1,29 @@
+package com.pda.core.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Alliance {
+
+    @Id
+    private Long id;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @OneToMany(mappedBy = "alliance")
+    @JsonIgnoreProperties("alliance")
+    private List<TravelerAlliance> travelerAlliances = new ArrayList<>();
+
+}
---
+++ core/src/main/java/com/pda/core/entity/Traveler.java
@@ -0,0 +1,45 @@
+package com.pda.core.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Traveler {
+
+    @Id
+    private Long id;
+
+    @Size(max=10)
+    private String nickname;
+
+    private String password;
+
+    private String role;
+
+    private Long stockballCount;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @OneToMany(mappedBy = "traveler")
+    @JsonIgnoreProperties("traveler")
+    private List<TravelerAlliance> travelerAlliances = new ArrayList<>();
+}
