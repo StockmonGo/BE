28a7fb1cf876571c936a4a45cc1ed867a3bbc9c9
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-08-29 15:22:02 +0900

    feat : 웹 소켓 세팅 관련 클래스 추가
    WebSocketMessageBrokerConfigurer 클래스 상속 받아 메서드 구현

---
+++ stock/src/main/java/com/pda/stock/websocket/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.pda.stock.websocket;
+
+import io.micrometer.observation.annotation.Observed;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+
+}
