bcc87c3ec418fed7d704500927a3f2cd3d8570f7
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-08-29 16:39:59 +0900

    fix: GetWorldStockmonsRequestDto - double로는 범위 체크가 되지 않는 오류가 있어 BigDecimal로 변경

--- core/src/main/java/com/pda/core/dto/GetWorldStockmonsRequestDto.java
+++ core/src/main/java/com/pda/core/dto/GetWorldStockmonsRequestDto.java
@@ -5,11 +5,10 @@ import jakarta.validation.constraints.DecimalMin;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import static com.pda.core.utils.WorldConstants.MAX_LATITUDE_STRING;
-import static com.pda.core.utils.WorldConstants.MAX_LONGITUDE_STRING;
-import static com.pda.core.utils.WorldConstants.MIN_LATITUDE_STRING;
+import java.math.BigDecimal;
+
 import static com.pda.core.exception.ExceptionMessage.OUT_OF_WORLD_RANGE;
-import static com.pda.core.utils.WorldConstants.MIN_LONGITUDE_STRING;
+import static com.pda.core.utils.WorldConstants.*;
 
 @Getter
 @RequiredArgsConstructor
@@ -17,10 +16,10 @@ public class GetWorldStockmonsRequestDto {
 
     @DecimalMin(value = MIN_LATITUDE_STRING, message = OUT_OF_WORLD_RANGE)
     @DecimalMax(value = MAX_LATITUDE_STRING, message = OUT_OF_WORLD_RANGE)
-    private final double latitude;
+    private final BigDecimal latitude;
 
-    @DecimalMax(value = MIN_LONGITUDE_STRING, message =  OUT_OF_WORLD_RANGE)
-    @DecimalMin(value = MAX_LONGITUDE_STRING, message =  OUT_OF_WORLD_RANGE)
-    private final double longitude;
+    @DecimalMin(value = MIN_LONGITUDE_STRING, message = OUT_OF_WORLD_RANGE)
+    @DecimalMax(value = MAX_LONGITUDE_STRING, message = OUT_OF_WORLD_RANGE)
+    private final BigDecimal longitude;
 
 }
