6064b9a63c4f50c043f928edabdc76245ba45b41
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-08-29 15:28:14 +0900

    feat : 웹 소켓을 통해 메시지를 브로커에게 전달하는 객체 추가

---
+++ stock/src/main/java/com/pda/stock/websocket/WebSocketMessage.java
@@ -0,0 +1,13 @@
+package com.pda.stock.websocket;
+
+import com.pda.stock.entity.Body;
+import com.pda.stock.entity.Header;
+
+public class WebSocketMessage {
+
+    public Header header;
+    public Body body;
+}
+
+
+
