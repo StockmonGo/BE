9c339f709bec50e8b58a6fffb50d67dc444fd003
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-08-29 15:27:04 +0900

    feat : 웹 소켓 메시지 컨트롤 클래스 추가

---
+++ stock/src/main/java/com/pda/stock/websocket/WebSocketEventListener.java
@@ -0,0 +1,32 @@
+package com.pda.stock.websocket;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pda.stock.entity.Body;
+import com.pda.stock.entity.Header;
+import com.pda.stock.entity.Input;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class WebSocketEventListener {
+
+
+
+
+
+}
