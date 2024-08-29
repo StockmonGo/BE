f2a97e1e5901229ac7a538838d1a01d252b9ac3a
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-08-29 16:42:38 +0900

    fix: StockTowerService - 이후 구현을 위해 StockTowerService의 메소드 파라미터 수정

--- core/src/main/java/com/pda/core/service/StockTowerService.java
+++ core/src/main/java/com/pda/core/service/StockTowerService.java
@@ -1,6 +1,7 @@
 package com.pda.core.service;
 
 
+import com.pda.core.dto.GetWorldStockmonsRequestDto;
 import com.pda.core.entity.StockTower;
 import com.pda.core.repository.StockTowerRepository;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +15,8 @@ public class StockTowerService {
 
     private final StockTowerRepository stockTowerRepository;
 
-    public List<StockTower> getNearStockTowers() {
+    public List<StockTower> getNearStockTowers(GetWorldStockmonsRequestDto getWorldStockmonsRequestDto) {
+
         return stockTowerRepository.findAll();
     }
 }
