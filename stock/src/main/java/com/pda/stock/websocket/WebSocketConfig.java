559b5771dea89878eae4e49d38bd9b108e54e115
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-08-29 15:23:04 +0900

    feat : 웹 소켓 브로커 경로와 destination 경로 설정

--- stock/src/main/java/com/pda/stock/websocket/WebSocketConfig.java
+++ stock/src/main/java/com/pda/stock/websocket/WebSocketConfig.java
@@ -12,5 +12,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config){
+        config.enableSimpleBroker("/topic","/queue","/content");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
 
 }
