752b39949e65a9d1516136ad35a5f06b1337da6c
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-01 15:42:38 +0900

    refactor: AccountController to TravelerController - 더 적절해 보이는 컨트롤러로 회원 정보 조회 API 이동

--- core/src/main/java/com/pda/core/controller/TravelerStockmonController.java
+++ core/src/main/java/com/pda/core/controller/TravelerStockmonController.java
@@ -6,13 +6,16 @@ import com.pda.core.dto.GetWorldStockmonsResponseDto;
 import com.pda.core.service.TravelerStockmonService;
 import io.swagger.v3.oas.annotations.Operation;
 import java.util.Date;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@Tag(name = "모험가_스톡몬 관련 API 모음")
+@RequestMapping("/api/core")
 public class TravelerStockmonController {
 
     private final TravelerStockmonService travelerStockmonService;
@@ -22,7 +25,7 @@ public class TravelerStockmonController {
     }
 
     @GetMapping("/stockmons")
-    @Operation(summary = "모험가의 스톡몬 목록 조회")
+    @Operation(summary = "모험가_스톡몬 목록 조회 API")
     public ResponseEntity<SuccessResponse<GetStockmonListResponseDto>> getStockmons() {
         // TODO: 추후 로그인 로직 추가시 수정
         long travelerId = 1;
