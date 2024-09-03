a421137244ecc318c895c817840c83cfc7cd67f0
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-03 16:44:03 +0900

    feat: TravelerController - 회원가입/로그인/회원탈퇴 Controller 추가

--- core/src/main/java/com/pda/core/controller/TravelerController.java
+++ core/src/main/java/com/pda/core/controller/TravelerController.java
@@ -1,39 +1,38 @@
 package com.pda.core.controller;
 
-import static com.pda.core.exception.ExceptionMessage.NO_TRAVELER;
 
 import com.pda.commons.dto.SuccessResponse;
-import com.pda.core.dto.GetTravelerNicknameResponseDto;
-import com.pda.core.dto.HasAccountResponseDto;
+import com.pda.core.dto.*;
 import com.pda.core.entity.Traveler;
 import com.pda.core.exception.NoTravelerException;
+import com.pda.core.service.CustomUserDetailService;
 import com.pda.core.service.TravelerService;
 import java.util.Date;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import static com.pda.core.config.SwaggerConfig.JWT;
+import static com.pda.core.config.SwaggerConfig.TRAVELER_ID;
 
 @RestController
 @Tag(name = "모험가 관련 API 모음")
+@RequiredArgsConstructor
 @RequestMapping("/api/core/users")
 public class TravelerController {
     private final TravelerService travelerService;
+    private final CustomUserDetailService customUserDetailService;
 
-    public TravelerController(TravelerService travelerService){
-        this.travelerService = travelerService;
-    }
 
-    @GetMapping
     @Operation(summary = "모험가 닉네임 검색 API")
+    @SecurityRequirement(name = JWT)
+    @GetMapping
     public ResponseEntity<SuccessResponse<GetTravelerNicknameResponseDto>> getTravelerByNickname(@RequestParam("name") String name) {
-        GetTravelerNicknameResponseDto traveler = travelerService.findTravelerByNickname(name).orElseThrow(() -> new NoTravelerException(
-                HttpStatus.BAD_REQUEST, NO_TRAVELER));
+        GetTravelerNicknameResponseDto traveler = travelerService.findTravelerByNickname(name).orElseThrow(NoTravelerException::new);
 
         return ResponseEntity.ok(SuccessResponse.<GetTravelerNicknameResponseDto>builder()
                 .data(traveler)
@@ -43,8 +42,10 @@ public class TravelerController {
         );
     }
 
-    @Operation(summary = "회원 정보 조회 API")
+
     @GetMapping("/profile")
+    @Operation(summary = "회원 정보 조회 API")
+    @SecurityRequirement(name = JWT)
     public ResponseEntity<SuccessResponse<HasAccountResponseDto>> getUserProfile() {
         // TODO: 추후 로그인 로직 추가시 수정
         long travelerId = 7;
@@ -59,6 +60,38 @@ public class TravelerController {
                 .build();
 
         return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "회원가입 API")
+    @PostMapping("/join")
+    public ResponseEntity<SuccessResponse<JoinResponseDto>> join(@RequestBody JoinRequestDto joinRequestDto) {
+        return ResponseEntity.ok().body(
+                SuccessResponse.<JoinResponseDto>builder()
+                        .message("생성 완료")
+                        .data(travelerService.createTraveler(joinRequestDto))
+                        .timestamp(new Date())
+                        .build());
+    }
+
+    @Operation(summary = "로그인 API")
+    @PostMapping("/signin")
+    public ResponseEntity<SuccessResponse<?>> signin(@RequestBody SignInRequestDto signInRequestDto) {
+        customUserDetailService.loadUserByUsername(signInRequestDto.getNickname());
+
+        return ResponseEntity.ok().body(null);
+    }
+
+    @DeleteMapping("/withdraw")
+    @Operation(summary = "회원탈퇴 API")
+    @SecurityRequirement(name = JWT)
+    public ResponseEntity<SuccessResponse<?>> withdraw(@RequestHeader(TRAVELER_ID) Long travelerId) {
+       travelerService.remove(travelerId);
+       return ResponseEntity.ok().body(SuccessResponse.<Object>builder()
+                       .data(null)
+                       .data(null)
+                       .message("회원 탈퇴 완료")
+                       .timestamp(new Date())
+               .build());
     }
 
 }
