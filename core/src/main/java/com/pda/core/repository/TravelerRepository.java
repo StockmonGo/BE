382533e144adaccce35541df6a0411a855625097
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-03 16:44:46 +0900

    feat: TravelerRepository - 보유 스톡볼 증가 시켜주는 로직 추가

--- core/src/main/java/com/pda/core/repository/TravelerRepository.java
+++ core/src/main/java/com/pda/core/repository/TravelerRepository.java
@@ -3,10 +3,17 @@ package com.pda.core.repository;
 import com.pda.core.entity.Traveler;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TravelerRepository extends JpaRepository<Traveler, Long> {
     Optional<Traveler> findByNickname(String nickname);
-    Optional<Traveler> findByAccountId(Long accountId);
+    Boolean existsByNickname(String nickname);
+
+    @Modifying
+    @Query("UPDATE Traveler t SET t.stockballCount = t.stockballCount + :ball WHERE t.nickname = :nickname")
+    void addStockball(@Param("nickname") String nickname, @Param("ball") Long ball);
 }
