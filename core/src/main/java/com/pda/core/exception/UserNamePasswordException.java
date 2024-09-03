a175b154f36ac50694b881ff4c90214bd38312af
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-03 17:06:45 +0900

    feat : Exception - 회원가입/로그인 로직 추가로 인한 예외 추가

---
+++ core/src/main/java/com/pda/core/exception/UserNamePasswordException.java
@@ -0,0 +1,11 @@
+package com.pda.core.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UserNamePasswordException extends CoreException{
+    private static final String NOT_CORRECT_USERNAME_PASSWORD = "비밀번호 불일치 | 아이디 불일치";
+
+    public UserNamePasswordException() {
+        super(HttpStatus.UNAUTHORIZED, NOT_CORRECT_USERNAME_PASSWORD);
+    }
+}
