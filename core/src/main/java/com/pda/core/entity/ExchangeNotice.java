d33e101b55ce73d82d1340010f83cb013cc95e4b
ChaeyoonWoo <heeni1004@gmail.com>
2024-09-04 15:38:09 +0900

    feat: AllianceNotice, TravelerNotice 엔티티 추가 및 날짜 관련 컬럼 삭제
    #38

---
+++ core/src/main/java/com/pda/core/entity/ExchangeNotice.java
@@ -0,0 +1,34 @@
+package com.pda.core.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExchangeNotice {
+
+    @Id
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Traveler sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Traveler receiver;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_stockmon_id")
+    private Stockmon senderStockmon;
+
+}
