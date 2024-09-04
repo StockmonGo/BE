c4925e24c88f9c84d4046d4918af04a0910fe782
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-09-04 15:44:24 +0900

    feat : 동맹 도메인 JpaRepository 인터페이스 추가

---
+++ core/src/main/java/com/pda/core/repository/AllianceRepository.java
@@ -0,0 +1,33 @@
+package com.pda.core.repository;
+
+import com.pda.core.dto.alliances.GetTravelerAlliancesListResponseDto;
+import com.pda.core.entity.TravelerAlliance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AllianceRepository extends JpaRepository<TravelerAlliance, Long> {
+
+    @Query(value = "SELECT t.id, t.nickname FROM traveler_alliance t1 " +
+            "INNER JOIN traveler_alliance t2 ON t1.alliance_id = t2.alliance_id " +
+            "AND t1.traveler_id != t2.traveler_id " +
+            "LEFT JOIN traveler t ON t2.traveler_id = t.id " +
+            "WHERE t1.traveler_id = :id", nativeQuery = true)
+    List<Object[]> findTravelerAllianceDataByTravelerId(Long id);
+
+    default Optional<List<GetTravelerAlliancesListResponseDto>> findNicknamesByAllianceId(Long id) {
+        List<Object[]> results = findTravelerAllianceDataByTravelerId(id);
+        List<GetTravelerAlliancesListResponseDto> dtos = results.stream()
+                .map(result -> new GetTravelerAlliancesListResponseDto(
+                        ((Number) result[0]).longValue(), // travelerId
+                        (String) result[1] // nickname
+                ))
+                .toList();
+
+        return Optional.ofNullable(dtos.isEmpty() ? null : dtos);
+    }
+}
