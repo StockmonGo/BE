0614115329c2ce4b42b90e48b68afe494b4a4de2
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-05 13:04:11 +0900

    feat: StockmonService - 스톡몬 상세 조회에 시가총액 추가

--- core/src/main/java/com/pda/core/service/StockmonService.java
+++ core/src/main/java/com/pda/core/service/StockmonService.java
@@ -1,11 +1,13 @@
 package com.pda.core.service;
 
 
+import com.pda.core.client.StockFeignClient;
 import com.pda.core.dto.GetStockmonDetailFromDbDto;
 import com.pda.core.dto.GetStockmonDetailResponseDto;
 import com.pda.core.entity.Stockmon;
 import com.pda.core.exception.NoStockmonDetailException;
 import com.pda.core.repository.StockmonRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +16,18 @@ import org.springframework.stereotype.Service;
 public class StockmonService {
 
     private final StockmonRepository stockmonRepository;
+    private final StockFeignClient stockFeignClient;
 
     public Stockmon getStockmon(long id) {
         return stockmonRepository.findById(id).orElseThrow();
     }
 
+    @Transactional
     public GetStockmonDetailResponseDto getStockmonDetail(Long id, Long travelerId) {
         GetStockmonDetailFromDbDto dbDto = stockmonRepository.findStockmonDetailById(id, travelerId)
                 .orElseThrow(NoStockmonDetailException::new);
 
-        return GetStockmonDetailResponseDto.from(dbDto);
+        return GetStockmonDetailResponseDto.from(dbDto, stockFeignClient.getTotalPrice(dbDto.getStockCode()).getBody());
     }
 
 }
