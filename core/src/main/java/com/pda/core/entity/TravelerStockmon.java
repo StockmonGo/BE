69e31d6ad23d01f53a2d9bb131cb28a3d3e79763
ChaeyoonWoo <heeni1004@gmail.com>
2024-09-06 08:51:10 +0900

    스톡몬 교환 요청 수락 관련 컨트롤러 작성

--- core/src/main/java/com/pda/core/entity/TravelerStockmon.java
+++ core/src/main/java/com/pda/core/entity/TravelerStockmon.java
@@ -19,7 +19,7 @@ public class TravelerStockmon {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     private Long stockmonCount;
 
     private Double stockmonAveragePrice;
