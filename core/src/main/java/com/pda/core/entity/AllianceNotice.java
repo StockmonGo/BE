137e8cf8fb03e7f5ab75a2b90c5120ede4d60a45
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-09-05 17:05:37 +0900

    fix : AllianceNotice 추가
    [수정 사항]
    - PK 값을 생성할 수 있도록 GeneratedValue 어노테이션 추가

--- core/src/main/java/com/pda/core/entity/AllianceNotice.java
+++ core/src/main/java/com/pda/core/entity/AllianceNotice.java
@@ -1,9 +1,6 @@
 package com.pda.core.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class AllianceNotice {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
