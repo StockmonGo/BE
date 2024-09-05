76bc1958526408aae5b574e66708eee52facfe4a
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-05 12:57:48 +0900

    refactor: Alliance - 변수명 변경

--- core/src/main/java/com/pda/core/service/AllianceService.java
+++ core/src/main/java/com/pda/core/service/AllianceService.java
@@ -1,6 +1,6 @@
 package com.pda.core.service;
 
-import com.pda.core.dto.alliances.GetTravelerAlliancesListResponseDto;
+import com.pda.core.dto.alliance.GetTravelerAllianceListResponseDto;
 import com.pda.core.repository.AllianceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,8 +17,8 @@ public class AllianceService {
 
 
     @Transactional
-    public List<GetTravelerAlliancesListResponseDto> getAlliances(Long allianceId){
-        List<GetTravelerAlliancesListResponseDto> travelerAllianceList = alliancesRepository.findNicknamesByAllianceId(1L).orElseThrow();
+    public List<GetTravelerAllianceListResponseDto> getAlliances(Long allianceId){
+        List<GetTravelerAllianceListResponseDto> travelerAllianceList = alliancesRepository.findNicknamesByAllianceId(1L).orElseThrow();
 
         return travelerAllianceList;
 
