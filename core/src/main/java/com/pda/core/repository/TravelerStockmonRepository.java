f796e1ad18827c2c62aef658f0667c2beb655517
ChaeyoonWoo <heeni1004@gmail.com>
2024-09-06 08:46:46 +0900

    TravelerStockmon 테이블 쿼리문 추가
    traveler.id와 stockmon.id가 모두 일치하는 행 찾는 쿼리문

--- core/src/main/java/com/pda/core/repository/TravelerStockmonRepository.java
+++ core/src/main/java/com/pda/core/repository/TravelerStockmonRepository.java
@@ -2,10 +2,18 @@ package com.pda.core.repository;
 
 import com.pda.core.entity.TravelerStockmon;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TravelerStockmonRepository extends JpaRepository<TravelerStockmon, Long > {
     List<TravelerStockmon> findByTravelerId(Long travelerId);
+
+    @Query("SELECT ts FROM TravelerStockmon ts WHERE ts. traveler. id = :travelerId AND ts. stockmon. id = :stockmonId")
+    Optional<TravelerStockmon> findByTravelerIdAndStockmonId(@Param("travelerId") Long travelerId,
+                                                             @Param("stockmonId") Long stockmonId);
+
 }
