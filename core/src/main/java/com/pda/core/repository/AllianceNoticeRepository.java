afaebdb1e1e94608d1861b36ef8d0faf195ee3c6
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-09-05 17:07:36 +0900

    feat : AllianceNoticeRepository 추가

---
+++ core/src/main/java/com/pda/core/repository/AllianceNoticeRepository.java
@@ -0,0 +1,9 @@
+package com.pda.core.repository;
+
+import com.pda.core.entity.AllianceNotice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AllianceNoticeRepository extends JpaRepository<AllianceNotice,Long> {
+
+
+}
