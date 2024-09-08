bae5dacd02381dbaad596e5fcbb5bfc24bfcfae1
ChaeyoonWoo <heeni1004@gmail.com>
2024-09-08 16:04:38 +0900

    tutorial_watched 여부 저장 서비스 로직 작성

--- core/src/main/java/com/pda/core/service/TravelerService.java
+++ core/src/main/java/com/pda/core/service/TravelerService.java
@@ -66,4 +66,12 @@ public class TravelerService {
 
     }
 
+    public void setTutorialWatched(Long travelerId) {
+        Traveler traveler = travelerRepository.findById(travelerId)
+                .orElseThrow((NoTravelerException::new));
+
+        traveler.setTutorialWatched(true);
+        travelerRepository.save(traveler);
+    }
+
 }
