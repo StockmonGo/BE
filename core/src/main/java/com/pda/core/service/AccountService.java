a198b753e12c2617014fd34eb281c5fc0a8b8e2c
ShinHeeEul <83682424+ShinHeeEul@users.noreply.github.com>
2024-09-08 15:59:00 +0900

    feat : AccountService - 실제 주식 교환 요청 API 서비스 작성

--- core/src/main/java/com/pda/core/service/AccountService.java
+++ core/src/main/java/com/pda/core/service/AccountService.java
@@ -1,27 +1,38 @@
 package com.pda.core.service;
 
 
+import com.pda.core.client.StockFeignClient;
 import com.pda.core.dto.AccountCheckResponseDto;
 import com.pda.core.dto.AccountDto;
 import com.pda.core.dto.HasAccountResponseDto;
 import com.pda.core.entity.Account;
+import com.pda.core.entity.AccountStock;
+import com.pda.core.entity.Stock;
 import com.pda.core.entity.Traveler;
 import com.pda.core.exception.HasAccountException;
+import com.pda.core.exception.NoAccountException;
+import com.pda.core.exception.NoStockException;
 import com.pda.core.exception.NoTravelerException;
 import com.pda.core.repository.AccountRepository;
+import com.pda.core.repository.AccountStockRepository;
+import com.pda.core.repository.StockRepository;
 import com.pda.core.repository.TravelerRepository;
 import java.util.UUID;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
     private final AccountRepository accountRepository;
     private final TravelerRepository travelerRepository;
+    private final StockFeignClient stockFeignClient;
+    private final AccountStockRepository accountStockRepository;
 
-    public AccountService(AccountRepository accountRepository, TravelerRepository travelerRepository){
-        this.accountRepository=accountRepository;
-        this.travelerRepository=travelerRepository;
-    }
+    private static final Integer STANDARD_PRICE = 1000;
+    private final StockRepository stockRepository;
 
     public Account createAccount(Long travelerId){
         Traveler traveler = travelerRepository.findById(travelerId)
@@ -61,6 +72,25 @@ public class AccountService {
     private String generateAccountNumber() {
         // TODO: 어떤 형식으로 계좌를 발급할지 고민
         return "000-" + UUID.randomUUID().toString().substring(0, 8);
+    }
+
+    @Transactional
+    public void changeRealStock(Long travelerId, String code) {
+        long currentPrice = stockFeignClient.getCurrentPrice(code).getBody();
+        double stockCount = 1.0 / (double) (currentPrice / STANDARD_PRICE);
+
+        Stock stock = stockRepository.findByCode(code).orElseThrow(NoStockException::new);
+        Account account = accountRepository.findByTravelerId(travelerId).orElseThrow(NoAccountException::new);
+
+        if(accountStockRepository.existsAccountStockByAccountIdAndStockId(account.getId(), stock.getId())) {
+            accountStockRepository.updateStock(account.getId(), stock.getId(), stockCount);
+        } else {
+            accountStockRepository.save(AccountStock.builder()
+                            .account(account)
+                            .stock(stock)
+                            .stockCount(stockCount)
+                    .build());
+        }
     }
 
 }
