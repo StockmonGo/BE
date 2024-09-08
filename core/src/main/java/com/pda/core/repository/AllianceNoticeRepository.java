adb2d65557d5dc83e8a94f7a338d1ab861f5c685
kmj-99 <76093968+kmj-99@users.noreply.github.com>
2024-09-08 13:18:40 +0900

    feat : 동맹 요청 목록 API DB 조회 쿼리문 추가

--- core/src/main/java/com/pda/core/repository/AllianceNoticeRepository.java
+++ core/src/main/java/com/pda/core/repository/AllianceNoticeRepository.java
@@ -1,9 +1,21 @@
 package com.pda.core.repository;
 
+import com.pda.core.dto.alliance_notice.GetAllianceNoticeListResponseDto;
 import com.pda.core.entity.AllianceNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface AllianceNoticeRepository extends JpaRepository<AllianceNotice,Long> {
 
-
+    @Query("SELECT an.id AS noticeId, t.nickname AS nickName " +
+            "FROM AllianceNotice an " +
+            "JOIN Traveler t ON an.sender.id = t.id " +
+            "WHERE an.receiver.id = :travelerId")
+    Optional<List<GetAllianceNoticeListResponseDto>> findAllianceNoticeListByTravelerId(@Param("travelerId") Long travelerId);
 }
